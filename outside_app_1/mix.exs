4f1c606b29ab304a92be55ac5697bf21c3061cc4
louisfield <louisfield@hotmail.com>
2023-04-11 18:10:17 +0800

    add umbrella outside dep call

--- outside_app_1/mix.exs
+++ outside_app_1/mix.exs
@@ -21,34 +21,34 @@ defmodule OutsideApp1.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, "~> 3.7.2"},
-      {:ecto_sql, "~> 3.7.2"},
-      {:postgrex, ">= 0.0.0"},
-      {:ecto_enum, "~> 1.4"},
-      # Jason for postgres
-      {:jason, "~> 1.2"},
-      {:poison, "~> 4.0"},
-      {:quantum, "~> 3.0"},
-      ## Money
-      {:decimal, "~> 1.9"},
-      {:ex_cldr, "~> 2.24"},
-      {:ex_money, "~> 5.9.0"},
-      {:ex_money_sql, "~> 1.4"},
-      {:countriex, "~> 0.4"},
-      {:argon2_elixir, "~> 2.4.0"},
-      {:dataloader, "~> 1.0.5"},
-      {:timex, "~> 3.5"},
-      {:mox, "~> 1.0", only: :test},
-      {:elasticsearch, "~> 1.0.0"},
-      # Testing
-      {:faker, "~> 0.15", only: :test},
-      ## Credo - static code analysis
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      # Retry
-      {:retry, "~> 0.14.1"},
-      {:outside_app_2, path: "../outside_app_2"},
-      {:poolboy, "~> 1.5.2"},
-      {:number, "~> 1.0"}
+      # {:ecto, "~> 3.7.2"},
+      # {:ecto_sql, "~> 3.7.2"},
+      # {:postgrex, ">= 0.0.0"},
+      # {:ecto_enum, "~> 1.4"},
+      # # Jason for postgres
+      # {:jason, "~> 1.2"},
+      # {:poison, "~> 4.0"},
+      # {:quantum, "~> 3.0"},
+      # ## Money
+      # {:decimal, "~> 1.9"},
+      # {:ex_cldr, "~> 2.24"},
+      # {:ex_money, "~> 5.9.0"},
+      # {:ex_money_sql, "~> 1.4"},
+      # {:countriex, "~> 0.4"},
+      # {:argon2_elixir, "~> 2.4.0"},
+      # {:dataloader, "~> 1.0.5"},
+      # {:timex, "~> 3.5"},
+      # {:mox, "~> 1.0", only: :test},
+      # {:elasticsearch, "~> 1.0.0"},
+      # # Testing
+      # {:faker, "~> 0.15", only: :test},
+      # ## Credo - static code analysis
+      # {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      # # Retry
+      # {:retry, "~> 0.14.1"},
+      {:outside_app_2, path: "../outside_app_2"}
+      # {:poolboy, "~> 1.5.2"},
+      # {:number, "~> 1.0"}
     ]
   end
 end
